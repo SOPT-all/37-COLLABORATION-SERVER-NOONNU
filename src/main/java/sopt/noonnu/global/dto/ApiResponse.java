228930d5687d45f3538b9150a2b70d7c9e3c2a9e
oOccasio <dhzktldh@gmail.com>
2025-11-17 20:38:56 +0900

    feat: #1 PageDto Add

--- src/main/java/sopt/noonnu/global/dto/ApiResponse.java
+++ src/main/java/sopt/noonnu/global/dto/ApiResponse.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 @JsonPropertyOrder({"errorCode", "message", "result"})
 public record ApiResponse<T>(
         String errorCode,
+
         String message,
+
         T result
 ) {
     public static <T> ApiResponse<T> success(T data) {
